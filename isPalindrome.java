876e7f37f7d13ffd553f17236d3e343dc5edc839
Fatih <88527389+blurout@users.noreply.github.com>
2022-01-23 21:30:17 -0800

    Solution 1
    Simple solution using for loops in JAVA.

---
+++ isPalindrome.java
@@ -0,0 +1,38 @@
+/**
+ * @author Blurout
+ */
+public class isPalindrome {
+	public static void main(String[] args) {
+		String word = "bub";
+		boolean TorF = check(word);
+		System.out.println(TorF);
+		if (TorF) {
+			System.out.println(word + " is a palindrome!\n" + word +" spelled backwards is: " + emordnilap(word));
+		} else {
+			System.out.println("the String is NOT a palindrome!\n" + word +" spelled backwards is: " + emordnilap(word));
+		}
+	}
+	public static boolean check(String word) {
+		word = word.toLowerCase();
+		String bword = emordnilap(word);
+		int len = word.length();
+		boolean b = false;
+		for (int i = 0; i < len; i++) {
+			if(word.charAt(i) == ' ') {
+				i++;
+				len--;
+			}
+			if (word.charAt(i) == bword.charAt(i)) {
+				b = true;
+				len--;
+			} else {
+				return false;
+			}
+		}
+		return b;
+	}
+	public static String emordnilap(String word) {
+		word = new StringBuilder(word).reverse().toString();
+		return word;
+	}
+}
